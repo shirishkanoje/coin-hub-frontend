a0dcd4a500324dea545922ac18e4cd1a57a64c40
shirishkanoje <shirishkanoje9@gmail.com>
2025-06-13 19:45:27 +0530

    change

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "https://mysql-javaversion.up.railway.app/"
+export const API_BASE_URL = "http://localhost:5454"
 
 
 const api=axios.create({
