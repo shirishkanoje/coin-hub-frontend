94ef470bd25da41458ac3a3b4155bfcbcb2f4b02
Shirish Kanoje <152640237+shirishkanoje@users.noreply.github.com>
2025-06-14 12:43:01 +0530

    backend live link updated

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "http://localhost:5454"
+export const API_BASE_URL = "https://coin-hub-backend-production.up.railway.app"
 
 
 const api=axios.create({
