3c07e40208a3469c30d55786ec70ac409d1712b0
Shirish Kanoje <shirishkanoje9@gmail.com>
2026-05-03 13:20:58 +0530

    Update Reducer.js

--- src/State/Coin/Reducer.js
+++ src/State/Coin/Reducer.js
@@ -1,5 +1,3 @@
-// src/redux/coinReducer.js
-
 import {
   FETCH_COIN_LIST_REQUEST,
   FETCH_COIN_LIST_SUCCESS,
@@ -34,80 +32,77 @@ const initialState = {
 
 const coinReducer = (state = initialState, action) => {
   switch (action.type) {
-      case FETCH_COIN_LIST_REQUEST:
-      case FETCH_TOP_50_COINS_REQUEST:
-      case FETCH_COIN_BY_ID_REQUEST:
-      case FETCH_COIN_DETAILS_REQUEST:
-      case SEARCH_COIN_REQUEST:
-          return { ...state, loading: true, error: null };
 
-      case FETCH_MARKET_CHART_REQUEST:
-          return {
-              ...state,
-              marketChart: { loading: true, data: [] },
-              error: null,
-          };
+    case FETCH_COIN_LIST_REQUEST:
+    case FETCH_TOP_50_COINS_REQUEST:
+    case FETCH_COIN_BY_ID_REQUEST:
+    case FETCH_COIN_DETAILS_REQUEST:
+    case SEARCH_COIN_REQUEST:
+      return { ...state, loading: true, error: null };
 
-      case FETCH_COIN_LIST_SUCCESS:
-          return { ...state,
-             coinList: action.payload,
-              loading: false,
-              error: null, 
-            };
+    case FETCH_MARKET_CHART_REQUEST:
+      return {
+        ...state,
+        marketChart: { loading: true, data: [] },
+        error: null,
+      };
 
-      case FETCH_TOP_50_COINS_SUCCESS:
-          return { ...state,
-             top50: action.payload,
-              loading: false,
-              error: null,
-             };
+    case FETCH_COIN_LIST_SUCCESS:
+      return {
+        ...state,
+        coinList: action.payload,
+        loading: false,
+        error: null,
+      };
 
-      case FETCH_MARKET_CHART_SUCCESS:
-          return {
-              ...state,
-              marketChart: { data: action.payload.prices, loading: false },
-                error: null,
-          };
+    case FETCH_TOP_50_COINS_SUCCESS:
+      return {
+        ...state,
+        top50: action.payload,
+        loading: false,
+        error: null,
+      };
 
-    //   case FETCH_COIN_BY_ID_SUCCESS:
-    //       return { ...state,
-    //          coinById: action.payload,
-    //           loading: false };
+    case FETCH_MARKET_CHART_SUCCESS:
+      return {
+        ...state,
+        marketChart: {
+          data: action.payload.prices,
+          loading: false,
+        },
+        error: null,
+      };
 
-      case SEARCH_COIN_SUCCESS:
-        return { 
-            ...state,
-           searchCoinList: action.payload.coins, 
-           loading: false ,
-           error: null,
-        };
+    case SEARCH_COIN_SUCCESS:
+      return {
+        ...state,
+        searchCoinList: action.payload.coins,
+        loading: false,
+        error: null,
+      };
 
-      case FETCH_COIN_DETAILS_SUCCESS:
-        return { 
-             ...state,
-             coinDetails: action.payload,
-             loading: false ,
-             error: null,
-        };
+    case FETCH_COIN_DETAILS_SUCCESS:
+      return {
+        ...state,
+        coinDetails: action.payload,
+        loading: false,
+        error: null,
+      };
 
-     case FETCH_MARKET_CHART_FAILURE:
-        return{
-            ...state,
-            marketChart: {loading: false,data:[]},
-            error: null,
-        };
+    case FETCH_COIN_LIST_FAILURE:
+    case FETCH_TOP_50_COINS_FAILURE:
+    case FETCH_MARKET_CHART_FAILURE:
+    case FETCH_COIN_BY_ID_FAILURE:
+    case FETCH_COIN_DETAILS_FAILURE:
+    case SEARCH_COIN_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
 
-     
-
-      case FETCH_COIN_LIST_FAILURE:
-      case FETCH_TOP_50_COINS_FAILURE:
-      case FETCH_MARKET_CHART_FAILURE:
-      case FETCH_COIN_BY_ID_FAILURE:
-      case FETCH_COIN_DETAILS_FAILURE:
-      case SEARCH_COIN_FAILURE:
-          return { ...state, loading: false, error: action.payload };
-      default:
-          return state;
+    default:
+      return state;
   }
 };
 
